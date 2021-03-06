9e48f4eda53ea64fd8f552ed53def0e63213e1b2
Benjamin Park <parkbenjamin2@gmail.com>
2021-03-06 14:44:46 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,116 @@
+const DATA = {
+	"genesis": {
+		"title": "Perqan Tōntanīn",
+		"content": [
+			{
+				"qn": `Tankā on Auhtōn ne ehkā nuhēn on tiyen in on tōmen.`,
+				"en": "In the beginning God made the heavens and the earth.",
+				"pr": `ˈtɑnkaː ɔn ˈaʊʔtoːn ne ɛʔˈkaː nʊʔˈeːn ɔn ˈtijən ɪn ɔn ˈtoːmən`
+			},
+			{
+				"qn": `Ehkā on tōmen issa istatūn in nordekā; ehkā ouhrīn ne meh rih tūrrēn, in osaxkā Lusah on Auhtōn ne meh rih alumi.`,
+				"en": `The world was without form and vacuous; darkness was upon the surface of the deep, and the Spirit of God moved above the surface of the waters.`,
+				"pr": `ɛʔˈkaː ɔn ˈtoːmən ˈɪssa ˈɪstatuːn ɪn ˈnɔrdekaː ɛʔˈkaː oʊʔriːn ne mɛʔ rɪʔ ˈtuːrreːn ɪn oˈsɑʃkaː ˈlusɑʔ ɔn ˈaʊʔtoːn ne mɛʔ rɪʔ ˈalumi`
+			},
+			{
+				"qn": `Irapkā on Auhtōn « Ehnā weskā rōen, » in tanrekā rōen.`,
+				"en": `God said "Let there be light," and there was made light.`,
+				"pr": `iˈrɑpkaː ɔn ˈaʊʔtoːn ɛʔˈnaː ˈhʷɛskaː ˈroːən ɪn ˈtɑnrekaː roːən`
+			},
+			{
+				"qn": `Imā on Auhtōn renā rōen, in ikāildā on Auhtōn tō ouhrīn rōen.`,
+				"en": `God saw that the light was good, and from the darkness God separated the light.`,
+				"pr": `iˈmaː ɔn ˈaʊʔtoːn reˈnaː ˈroːən ɪn iˈkaːɪldaː ɔn ˈaʊʔtoːn toː ˈoʊʔriːn roːən`
+			},
+			{
+				"qn": `Ipīlkā on Auhtōn rōen ko maxtūn, in ipīlkā ouhrīn ko ēqinen. Ehkā ān esxemēn in pouhmen maxtūn dimmal.`,
+				"en": `God called the light day, and called the darkness night. And so evening and morning was the first day.`,
+				"pr": `iˈpiːɫkaː ɔn ˈaʊʔtoːn ˈroːən ko ˈmɑʃtuːn ɪn iˈpiːɫkaː ˈoʊʔrɪn ko ˈeːkʷinən ɛʔˈkaː aːn ˈɛsʃemeːn ɪn ˈpoʊʔmən ˈmɑʃtuːn ˈdɪmmɑɫ`
+			}
+		]
+	},
+	"babel": {
+		"title": "Kurōken ne Pāpelen (Tōntanīn 11)",
+		"content": [
+			{
+				"qn": "Ehkā inqē ya on tōmen din nawēn in din asra nawīn.",
+				"en": `And so the world had one language and one possibility for speech.`,
+				"pr": `ɛʔˈkaː ˈɪnkʷeː ja ɔn ˈtoːmən dɪn naˈweːn ɪn dɪn ˈɑsra naˈwiːn`
+			},
+			{
+				"qn": `In losaxkā, ticūrīhcekiē tō tozwān, yapulkā pēxmazūen ne mazū on Xināren; in tiqartekā ātuecēn.`,
+				"en": `And it happened, as they had been journeying from the east, that they found a plain in the land of Shinar; and in there they dwelt.`,
+				"pr": `ɪn loˈsɑʃkaː titʃuːriːʔˈtʃekieː toː ˈtɔtsʍaːn jaˈpʊɫkaː peːʃmaˈtsuːən ne maˈtsuː ɔn ʃiˈnaːrən ɪn tiˈkʷɑrtekaː aːˈtuetʃeːn`
+			},
+			{
+				"qn": `In tirapkā yaqō ān « Ehnā zitanā esxizē in azwouhlā millē. » In yahū entē izen esxizen, entē pestōn lakren.`,
+				"en": `And they said to each other "Let us make bricks and burn them wholly." And they had bricks for stone, tar for mortar.`,
+				"pr": `ɪn tiˈrɑpkaː jaˈkʷoː aːn ɛʔˈnaː ˈtsitanaː ˈɛsʃitseː ɪn ɑtsˈʍoʊʔlaː ˈmɪɫleː ɪn jɑʔˈuː ˈɛnteː ˈitsən ˈɛsʃitsən ˈɛnteː pɛsˈtoːn ˈlɑkrən`
+			},
+			{
+				"qn": `Tirapkā ne ātin lōn « Ehnā zitanā ko oren iztanēn ya nay kurōken odetasal sū on Tiyen ko xumi zixā ulawnā on tōmen solpīlē; īs losaxā, azsayterā ne warrih passa on tōmen. »`,
+				"en": `At that time they said "Let us make for ourselves a city that has a tower able to reach unto Heaven so that we may make the world know our names; if this does not pass, we will be scattered across the face of all the earth."`,
+				"pr": `tiˈrɑpkaː ne ˈaːtɪn loːn ɛʔˈnaː ˈtsitanaː ko ˈorən ˈɪtstaneːn ja nɑj kuˈroːkən oˈðetasɑɫ suː ɔn ˈtijən ko ˈʃumi tsiˈʃaː uˈlɑʍnaː ɔn ˈtoːmən sɔlˈpiːleː iːs ˈlosaʃaː ɑtsˈsɑjteraː ne ˈhʷɑrrɪʔ ˈpɑssa ɔn ˈtoːmən`
+			},
+			{
+				"qn": `Pa ūhkā sūwa on Auhtōn ko xumi imā iztanēn in kurōken tutancekal ten sotē.`,
+				"en": `But God came to them to see the city and tower that these people had been making.`,
+				"pr": `pa ˈuːʔkaː ˈsuːwa ɔn ˈaʊʔtoːn ko ˈʃumi iˈmaː ˈɪtstaneːn ɪn kuˈroːkən tuˈtɑntʃekɑɫ tɛn soˈteː`
+			},
+			{
+				"qn": `Irapkā on Auhtōn « Titannuhasō tiē din medra sotē, tā samī on tixumal on īs teyasal.`,
+				"en": `God said "If they are able to make this being one breadth of people, there will not be anything which they desire that they cannot do.`,
+				"pr": `iˈrɑpkaː ɔn ˈaʊʔtoːn titannʊʔˈasoː tiˈeː dɪn mɛt̚ra soˈteː taː saˈmiː ɔn ˈtiʃumɑɫ ɔn iːs ˈtejasɑɫ`
+			},
+			{
+				"qn": `Ehnā zahsaxā ahcemiē in azyukictayā unaw ko xumi īs tahnawasā ya opos. »`,
+				"en": `Let us come near and break apart their language so that they are unable to speak with each other."`,
+				"pr": `ɛʔˈnaː tsɑʔˈsaʃaː ɑʔˈtʃemieː ɪn ɑtsjuˈkɪtʃtajaː ˈunɑʍ ko ˈxumi iːs tɑʔˈnawasaː ja ˈopɔs`
+			},
+			{
+				"qn": `Useytekā on Auhtōn inqē tō ātuecēn ne passa on tōmen in estekā tani iztanēn.`,
+				"en": `And so God scattered them from there across the entire earth and construction of the city was stopped.`,
+				"pr": `uˈsɛjtekaː ɔn ˈaʊʔtoːn ˈɪnkʷeː toː aːˈtuetʃeːn ne ˈpɑssa ɔn ˈtoːmən ɪn ˈɛstekaː ˈtani ˈɪtstaneːn`
+			},
+			{
+				"qn": `Cēz pīllā ko Pāpelen tīen; yukictaykā ne ātuecēn naw passa on tōmen. Teseyterkā tō ātuecēn ne warrih passa on tōmen mo on Auhtōn.`,
+				"en": `This is the reason it is called Babel; the language of the entire world was dissolved there. They were scattered from there by God upon the face of the entire earth.`,
+				"pr": `tʃeːts ˈpiːɫlaː ko ˈpaːpelən ˈtiːən juˈkɪtʃtɑjkaː ne aːˈtuetʃeːn nɑʍ ˈpɑssa ɔn toːmən teˈsɛjtərkaː toː aːˈtuetʃeːn ne ˈhʷɑrrɪʔ ˈpɑssa ɔn ˈtoːmən mo ɔn ˈaʊʔtoːn`
+			}
+		]
+	}
+}
+
+function transliterate(s) {
+	return s.replace(/(w)([aeiouāēīōū])/g, 'hu$2').replace('q', 'qu');
+}
+
+function genVerse(n, data, isFirst) {
+	let s = `<div class="hoverable">`;
+	if (data.en != undefined) {
+		s += `<span class="trnsl"><p class="en">${data.en}</p>`;
+		s += `<p class="pro">${transliterate(data.qn)}</p>`;
+		if (data.pr != undefined)
+			s += `<p class="ipa">[${data.pr}]</p>`;
+		s += `</span>`;
+	}
+	if (isFirst) {
+		s += `<span class="fc">${data.qn.charAt(0)}</span>${data.qn.substring(1)}`
+	} else {
+		s += data.qn;
+	}
+	s += `<span class="num">${n}</span></div>`;
+	return s;
+}
+
+function genPassage(data) {
+	let s = `<div class="para"><h1>${data.title}</h1><p>`;
+	data.content.forEach((c, i) => {
+		s += genVerse(i + 1, c, i == 0);
+	});
+	return s + "</p></div>";
+}
+
+window.onload = function () {
+	document.getElementById("text").innerHTML = genPassage(DATA["genesis"]) + genPassage(DATA["babel"]);
+}
